ba429fb0f18e3f959fd373ec5b7dced502b59c03
Sushan Paudyal <cto@onlinezeal.com>
2018-12-26 12:04:40 +0545

    Initial Commit and Login Panel Made

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -45,6 +45,23 @@
                     <span class="db"><img src="{{asset('public/adminpanel/assets/images/logo-icon.png')}}" alt="logo" /></span>
                     <h5 class="font-medium m-b-20">Sign In to Admin</h5>
                 </div>
+
+
+                <div id="loginbox">
+                  @if(Session::has('flash_message_error'))
+                      <div class="alert alert-danger alert-block">
+                          <button type="button" class="close" data-dismiss="alert">X</button>
+                          <strong class="text-danger">{!! session('flash_message_error') !!}</strong>
+                      </div>
+                  @endif
+
+                  @if(Session::has('flash_message_success'))
+                      <div class="alert alert-success alert-block">
+                          <button type="button" class="close" data-dismiss="alert">X</button>
+                          <strong class="text-success">{!! session('flash_message_success') !!}</strong>
+                      </div>
+                  @endif
+                </div>
                 <!-- Form -->
                 <div class="row">
                     <div class="col-12">
@@ -76,7 +93,7 @@
                                     <button class="btn btn-block btn-lg btn-info" type="submit">Log In</button>
                                 </div>
                             </div>
-                            
+
 
                         </form>
                     </div>
