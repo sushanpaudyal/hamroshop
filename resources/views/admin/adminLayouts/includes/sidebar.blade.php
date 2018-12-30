159ccc81b717d57dc5b6824c5442b19922b4f9df
Sushan Paudyal <cto@onlinezeal.com>
2018-12-30 11:33:56 +0545

    changes in sidebar

--- resources/views/admin/adminLayouts/includes/sidebar.blade.php
+++ resources/views/admin/adminLayouts/includes/sidebar.blade.php
@@ -42,35 +42,30 @@
                     <!-- End User Profile-->
                 </li>
                 <!-- User Profile-->
-                <li class="nav-small-cap">
-                    <i class="mdi mdi-dots-horizontal"></i>
-                    <span class="hide-menu">Personal</span>
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('admin.dashboard')}}" aria-expanded="false">
+                        <i class="mdi mdi-content-paste"></i>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
                         <i class="icon-Car-Wheel"></i>
-                        <span class="hide-menu">Dashboards </span>
+                        <span class="hide-menu">Category</span>
                     </a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item">
                             <a href="index.html" class="sidebar-link">
                                 <i class="icon-Record"></i>
-                                <span class="hide-menu"> Dashboard 1 </span>
+                                <span class="hide-menu">Create</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
                             <a href="index2.html" class="sidebar-link">
                                 <i class="icon-Record"></i>
-                                <span class="hide-menu"> Dashboard 2 </span>
+                                <span class="hide-menu">View</span>
                             </a>
                         </li>
-                        <li class="sidebar-item">
-                            <a href="index3.html" class="sidebar-link">
-                                <i class="icon-Record"></i>
-                                <span class="hide-menu"> Dashboard 3 </span>
-                            </a>
-                        </li>
-
                     </ul>
                 </li>
 
