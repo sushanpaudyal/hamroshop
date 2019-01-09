afeff854dfaf6b2867c9b715c1133e19b6572ae6
Sushan Paudyal <cto@onlinezeal.com>
2019-01-09 12:09:10 +0545

    Frontend Setup

---
+++ resources/views/frontend/frontLayouts/front_header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Hamro Shop -  J Kunni Cha Hami Chau</title>
+    <link href="{{asset('public/frontpanel/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontpanel/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontpanel/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontpanel/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontpanel/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontpanel/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('public/frontpanel/css/responsive.css')}}" rel="stylesheet">
+
+</head><!--/head-->
