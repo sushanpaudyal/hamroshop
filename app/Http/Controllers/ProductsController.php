b90fbbbbf8b67fa02f07f0ccdf73e6f75d761940
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-01-27 12:08:35 +0545

    Adding to Cart Items with session id

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -365,20 +365,29 @@ class ProductsController extends Controller
             $data['user_email'] = "";
         }
 
-        if(empty($data['session_id'])){
-            $data['session_id'] = "";
+        $session_id = Session::get('session_id');
+        if(empty($session_id)){
+            $session_id = str_random(40);
+            Session::put('session_id', $session_id);
         }
 
         $sizeArr = explode("-", $data['size']);
 
-        DB::table('carts')->insert(['product_id' => $data['product_id'] , 'product_name' => $data['product_name'] , 'product_code' => $data['product_code'], 'product_color' => $data['product_color'], 'price' => $data['price'], 'size' => $sizeArr[1], 'quantity' => $data['quantity'], 'user_email' => $data['user_email'], 'session_id' => $data['session_id']
+        DB::table('carts')->insert(['product_id' => $data['product_id'] , 'product_name' => $data['product_name'] , 'product_code' => $data['product_code'], 'product_color' => $data['product_color'], 'price' => $data['price'], 'size' => $sizeArr[1], 'quantity' => $data['quantity'], 'user_email' => $data['user_email'], 'session_id' => $session_id
         ]);
 
+        return redirect()->route('cart')->with('flash_message_error', 'Product Has Been Added to Cart');
     }
 
 
     public function cart(){
-        return view ('products.cart');
+        $session_id = Session::get('session_id');
+        $userCart = DB::table('carts')->where(['session_id' => $session_id])->get();
+        foreach($userCart as $key => $product){
+            $productDetails = Product::where('id' , $product->product_id)->first();
+            $userCart[$key]->image = $productDetails->image;
+        }
+        return view ('products.cart', compact('userCart'));
     }
 
 }
